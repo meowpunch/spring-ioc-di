28229d70cae62b90b7b8a4d6e232c335237282e1
meowpunch <jinhoonbae@gmail.com>
2022-03-31 22:12:52 +0200

    spring profile

--- src/main/java/com/example/iocdi/diWithSpring/service/KoreanGreetingService.java
+++ src/main/java/com/example/iocdi/diWithSpring/service/KoreanGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 
-@Profile("KR")
+@Profile({"KR", "default"})
 @Service("i18nService")
 public class KoreanGreetingService implements GreetingService {
     @Override
