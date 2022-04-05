ae060eee88c055e4653d6bc66dc281d91c4c2b69
meowpunch <jinhoonbae@gmail.com>
2022-04-05 19:55:04 +0200

    bean life cycle

--- src/main/java/com/example/iocdi/diWithSpring/controller/MyController.java
+++ src/main/java/com/example/iocdi/diWithSpring/controller/MyController.java
@@ -1,10 +1,17 @@
 package com.example.iocdi.diWithSpring.controller;
 
 import com.example.iocdi.diWithSpring.service.GreetingService;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MyController {
+public class MyController implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
     final private GreetingService greetingService;
 
@@ -15,5 +22,20 @@ public class MyController {
 
     public String getGreeting() {
         return greetingService.sayHello();
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("## My Bean Name is: " + name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("## Bean Factory has been set");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("## Application context has been set");
     }
 }
